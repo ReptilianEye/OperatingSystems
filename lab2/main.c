20d151b877a0f114ada5a31ee9261a6aef43feed
ReptilianEye <piotr.rzadkowski@outlook.com>
2024-03-22 17:16:21 +0100

    lab2 cleanup

--- lab2/main.c
+++ lab2/main.c
@@ -1,3 +1,34 @@
+#ifdef dynamic
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+int main()
+{
+    char *error;
+    void *handle = dlopen("libcollatz.so", RTLD_LAZY);
+    if (!handle)
+    {
+        fputs(dlerror(), stderr);
+        exit(1);
+    }
+
+    int (*convergence)(int, int);
+    convergence = dlsym(handle, "test_collatz_convergence");
+
+    if ((error = dlerror()) != NULL)
+    {
+        fputs(error, stderr);
+        exit(1);
+    }
+    int res = (*convergence)(15, 100);
+    printf("Result: %d\n", res);
+
+    dlclose(handle);
+}
+
+#else
 #include <stdio.h>
 #include "collatz.h"
 
@@ -7,3 +38,5 @@ int main()
     printf("Result: %d\n", result);
     return 0;
 }
+
+#endif
